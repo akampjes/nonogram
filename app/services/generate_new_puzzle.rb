9eeb009491ef23744eade8ad5663067ad811c7fd
Andrew Kampjes <andrew.kampjes@powershop.co.nz>
2015-09-08 08:53:23 +1200

    Testing that presenter produces something sane

--- app/services/generate_new_puzzle.rb
+++ app/services/generate_new_puzzle.rb
@@ -9,6 +9,9 @@ class GenerateNewPuzzle
     grid.randomly_populate!
     grid.create_clues!(@game)
 
+    # this means that I've now moved everything from a service back into a model.
+    # I could probably do create_clues here.
+
     print grid.display_string(@game)
   end
 end
--- spec/presenters/game_grid_presenter_spec.rb
+++ spec/presenters/game_grid_presenter_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe GameGridPresenter do
+  let(:board_size) { 5 }
+  let(:game) { Game.create!(board_size: board_size) }
+
+  it 'is an Array of Arrays of Tiles' do
+    # Should look something like:
+    # [ [Tile, Tile], [Tile, Tile] ]
+
+    GenerateNewPuzzle.new(game: game).call
+    presented_game = GameGridPresenter.new(game)
+
+    expect(presented_game.rows).to be_kind_of Array
+    
+    presented_game.rows.each do |row|
+      expect(row).to be_kind_of Array
+
+      row.each do |tile|
+        expect(tile).to be_kind_of Tile
+      end
+    end
+  end
 end
