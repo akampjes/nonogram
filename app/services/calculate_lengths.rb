7af01ca243f7d5e496d784eceeaf3cb8a6889119
Andrew Kampjes <andrew.kampjes@powershop.co.nz>
2015-09-12 00:19:30 +1200

    The worlds shortest service...

--- app/services/calculate_lengths.rb
+++ app/services/calculate_lengths.rb
@@ -4,24 +4,6 @@ class CalculateLengths
   end
 
   def call
-    calculate_lengths(@line)
-  end
-
-  def calculate_lengths(line)
-    #line.to_a.split.reject(&:empty?).map(&:count).to_a
-    lengths = []
-
-    run = 0
-    line.each do |tile|
-      if tile
-        run += 1
-      elsif run > 0
-        lengths << run
-        run = 0
-      end
-    end
-    lengths << run if run > 0
-
-    lengths
+    @line.split(&:blank?).reject(&:empty?).map(&:count)
   end
 end
--- spec/services/calculate_lengths_spec.rb
+++ spec/services/calculate_lengths_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe CalculateLengths do
   it 'works out the correct lengths' do
-    line = [nil, true, true, nil, true]
+    line = [false, true, true, false, true]
     expect(CalculateLengths.new(line: line).call).to eq [2, 1]
   end
 
@@ -12,7 +12,7 @@ RSpec.describe CalculateLengths do
   end
 
   it 'handles no lengths' do
-    line = [nil, nil, nil]
+    line = [false, false, false]
     expect(CalculateLengths.new(line: line).call).to eq []
   end
 end
